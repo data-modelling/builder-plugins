f7e7e54932fe0954c71afcd69a11e404e34a4a5d
Tero <tero@starupcommons.org>
2020-07-26 09:49:51 +0200

    core test

--- packages/builder-core/src/index.js
+++ packages/builder-core/src/index.js
@@ -1,8 +1,7 @@
-
 import React from "react";
 
-export * from './graphQLHelpers'
+export * from "./graphQLHelpers";
 
-export const ObjectTest={"test":"something"};
-export const ComponentTest=<div>Testing React Component</div>
+export const ObjectTest = { test: "something", new: "test" };
+export const ComponentTest = <div>Testing React Component</div>;
 //export * from './tools'

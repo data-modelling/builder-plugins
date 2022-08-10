e385826550dbab2a8f48ddfea65711e2ab237eef
prifina-admin <tero@prifina.com>
2022-08-10 13:12:01 +0300

    dist included

--- packages/json-view/src/App.js
+++ packages/json-view/src/App.js
@@ -1,13 +1,17 @@
 import JsonViewPlugin from "./JsonViewPlugin";
 import React from "react";
-
+import {
+ HashRouter,
+} from "react-router-dom";
 
 //const App = forwardRef(({theme={},schema={ test: "Testing2" }},ref) => {
 
 const App = ({theme={},schema={ test: "Testing2" }}) => (
   <div>
     <h1>Plugin Test</h1>
+    <HashRouter>
     <JsonViewPlugin theme={theme} schema={schema} />
+    </HashRouter>
   </div>
 );
 
--- packages/json-view/src/JsonViewPlugin.js
+++ packages/json-view/src/JsonViewPlugin.js
@@ -1,6 +1,15 @@
 
 import React from "react";
 import ReactJson from 'react-json-view';
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useParams,
+  useLocation,
+  useNavigate
+} from "react-router-dom";
 
 const linkStyle = {
   marginBottom: '20px',
@@ -10,17 +19,57 @@ const linkStyle = {
   }
 };
 
+const RELATED_ID = 1;
+
+const MyComponent = ({ title }) => {
+  const { pathname } = useLocation();
+  const { id } = useParams();
+  console.log("PLUGIN ROUTE ",pathname,id);
+  return (
+    <div>
+      <h1>
+        {title} {id}
+      </h1>
+      <pre>{pathname}</pre>
+      {id && RELATED_ID && (
+        <Link to={`${pathname}/../${RELATED_ID}`}>
+          Go to Nested Route {RELATED_ID}
+        </Link>
+      )}
+    </div>
+  );
+};
 export default function JsonViewPlugin(props) {
-    
+  //const location = useLocation();
+ 
+  const navigate=useNavigate();
+ console.log("PLUGIN ",location);
     const exportData = new Blob([ JSON.stringify(props.schema) ], { type: 'application/json' });
     const exportUrl = window.URL.createObjectURL(exportData);
     return (
-        <React.Fragment>
+        <>
+        <button onClick={()=>{
+          navigate("/route-bb",{replace:true})
+        }}>NAV TO /route-bb</button>
+        <nav>
+        <Link to="/route-aa">Route A</Link>
+          <Link to="/route-bb">Route B</Link>
+        </nav>
+        <Routes>
+          <Route
+            path="/route-bb"
+            element={<MyComponent title="Nested Route" />}
+          />
+          <Route
+            path="/route-aa"
+            element={<MyComponent title="Nested Route" />}
+          />
+         </Routes> 
           <div style={linkStyle}>
           <a href={exportUrl} target="_blank"  download="schema.json" rel="noopener noreferrer"  >Export</a>
           </div>
            <ReactJson src={props.schema} collapsed={false}/>
-       </React.Fragment>
+       </>
         
     )    
 
